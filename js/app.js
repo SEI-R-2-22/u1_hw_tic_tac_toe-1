e0446c0dbaac321d8e096f260581512caf72fe5b
Nick Lopez <nicklopez5454@yahoo.com>
2022-02-28 10:49:47 -0600

    Wrote AI

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let board = document.querySelector('.game-board')
 const squares = document.querySelectorAll('.square')
 let turnDisplay = document.getElementById('turn')
 let playerTurn = 1
+let computerPick
 let winState = 0
 let replayBtn = document.createElement('button')
 xWins = 0
@@ -23,21 +24,22 @@ const youWin = () => {
   document.querySelector('h1').innerHTML = 'Game!'
   document.getElementById('turn-board').style.opacity = '0'
   winState = 1
-  if (playerTurn === 0) {
+  if (playerTurn === 1) {
     document.getElementById('winner').innerHTML = 'X Wins!'
     tallyIncrementor()
-  } else if (playerTurn === 1) {
+  } else if (playerTurn === 0) {
     document.getElementById('winner').innerHTML = 'O Wins!'
+    playerTurn = 0
     tallyIncrementor()
   }
   document.body.appendChild(replayBtn)
 }
 
 const tallyIncrementor = () => {
-  if (playerTurn === 0 && winState === 1) {
+  if (playerTurn === 1 && winState === 1) {
     xWins++
     document.getElementById('x-win-count').innerHTML = xWins
-  } else if (playerTurn === 1 && winState === 1) {
+  } else if (playerTurn === 0 && winState === 1) {
     oWins++
     document.getElementById('o-win-count').innerHTML = oWins
   }
@@ -47,6 +49,7 @@ const youTie = () => {
   document.getElementById('turn-board').style.opacity = '0'
   document.querySelector('h1').innerHTML = 'Draw!'
   document.body.appendChild(replayBtn)
+  winState++
 }
 
 const checkForWin = () => {
@@ -112,21 +115,53 @@ const checkForWin = () => {
     youTie()
   }
 }
+const computerTurn = () => {
+  computerPick = Math.floor(Math.random() * 8)
+  if (squares[computerPick].innerHTML === '' && winState === 0) {
+    playerTurn--
+    squares[computerPick].innerHTML = '0'
+    checkForWin()
+    playerTurn++
+    console.log(`computer chose square ${computerPick}`)
+  } else if (squares[computerPick].innerHTML != '' && winState === 0) {
+    computerTurn()
+  }
+}
+
 ////////////////////////////////
 // Event Listeners Here
+
+// for (let i = 0; i < squares.length; i++) {
+//   squares[i].addEventListener('click', () => {
+//     if (playerTurn === 1 && squares[i].innerHTML === '' && winState === 0) {
+//       squares[i].innerHTML = 'X'
+//       playerTurn--
+//     } else if (
+//       playerTurn === 0 &&
+//       squares[i].innerHTML === '' &&
+//       winState === 0
+//     ) {
+//       squares[i].innerHTML = 'O'
+//       playerTurn++
+//     }
+//     if (winState === 0) {
+//       checkForWin()
+//     }
+//     scoreBoardDisplayer()
+//     console.log(`win state is ${winState}`)
+//     console.log(`player turn is ${playerTurn}`)
+//     console.log(`X won ${xWins} times`)
+//     console.log(`0 won ${oWins} times`)
+//   })
+// }
 
 for (let i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', () => {
     if (playerTurn === 1 && squares[i].innerHTML === '' && winState === 0) {
       squares[i].innerHTML = 'X'
-      playerTurn--
-    } else if (
-      playerTurn === 0 &&
-      squares[i].innerHTML === '' &&
-      winState === 0
-    ) {
-      squares[i].innerHTML = 'O'
-      playerTurn++
+      checkForWin()
+
+      computerTurn()
     }
     if (winState === 0) {
       checkForWin()
@@ -152,6 +187,7 @@ replayBtn.addEventListener('click', () => {
   document.querySelector('h1').innerHTML = ''
   document.getElementById('turn-board').style.opacity = '1'
   document.getElementById('winner').innerHTML = ''
+  playerTurn = 1
   winState = 0
   replayBtn.remove()
 })
